5e0c9fdadf123d2437c07e122822bb8c8da5b79b
Guilherme Mattos <guilhermevsmattos@outlook.com>
2020-01-23 22:05:59 -0300

    Added Form style

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Image, View, Text, TextInput } from 'react-native';
+import { StyleSheet, Image, View, Text, TextInput, TouchableOpacity } from 'react-native';
 import MapView, { Marker, Callout } from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
+import { MaterialIcons } from '@expo/vector-icons';
 
 function Main({ navigation }) {
   const [currentRegion, setCurrentRegion] = useState(null);
@@ -51,6 +52,20 @@ function Main({ navigation }) {
           </Callout>
         </Marker>
       </MapView>
+
+      <View style={styles.searchFrom}>
+          <TextInput 
+            style={styles.searchInput}
+            placeholder="Buscar devs por techs..."
+            placeholderTextColor="#999"
+            autoCapitalize="words"
+            autoCorrect={false}
+          />
+
+          <TouchableOpacity onPress={() => {}} style={styles.loadButton}>
+            <MaterialIcons name="my-location" size={20} color="#FFF" />
+          </TouchableOpacity>
+      </View>
     </>
   );
 }
@@ -84,6 +99,41 @@ const styles = StyleSheet.create({
   devTechs: {
     marginTop: 5,
   },
+
+  searchFrom: {
+    position: 'absolute',
+    top: 20,//TODO deixar no bottom e fazer o form subir
+    left: 20,
+    right: 20,
+    zIndex: 5,
+    flexDirection: 'row',
+  },
+
+  searchInput: {
+    flex: 1,
+    height: 50,
+    backgroundColor: '#FFF',
+    borderRadius: 25,
+    paddingHorizontal: 20,
+    fontSize: 16,
+    shadowColor: '#000',
+    shadowOpacity: 0.2,
+    shadowOffset: {
+      width: 4,
+      height: 4,
+    },
+    elevation: 3,
+  },
+
+  loadButton: {
+    width: 50,
+    height: 50,
+    backgroundColor: '#8E4DFF',
+    borderRadius: 25,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 15,
+  }
 
 });
 
